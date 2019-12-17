45cb3f96d3f0a028230979e5eea6ce78033424b2
ppambler <allinlbj@gmail.com>
2019-12-17 14:03:51 +0800

    .babelrc 即 babel-loader的配置

--- webpack.config.js
+++ webpack.config.js
@@ -12,22 +12,7 @@ module.exports = {
       {
         test: /\.js$/,
         use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: {
-                    browsers: ["> 1%", "last 2 versions", "not ie <= 8"]
-                  }
-                }
-              ]
-            ],
-            plugins: [
-              ['@babel/transform-runtime']
-            ]
-          }
+          loader: 'babel-loader'
         }
       }
     ]
